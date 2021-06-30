b491c56a9bf8398cf436202beeb77d4c9e8e72f0
thomazfranca <trsf1990@gmail.com>
2021-06-30 18:23:10 -0300

    Relacionamento entre Enity Springboot

--- springboot/blogpessoal/src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
+++ springboot/blogpessoal/src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -6,10 +6,9 @@ import org.generation.blogpessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
-	public LinkedList<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
-	
+	public LinkedList<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+
 }
